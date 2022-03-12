091d0fef5abb93b375a8825c741003e3537d6dbd
Kendall Harter <kendallremna@protonmail.com>
2022-03-12 09:10:01 -0800

    Better error handling!

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 #include <iterator>
 #include <string_view>
 #include <type_traits>
+#include <climits>
 
 template<std::size_t N>
 struct comp_str {
@@ -17,69 +18,15 @@ struct comp_str {
       }
    }
 
-   char data[N];
-   constexpr auto operator<=>(const comp_str&) const = default;
-};
-
-template<comp_str str>
-consteval auto test()
-{
-   const auto& data = str.data;
-   std::array<std::pair<std::string_view, std::string_view>, std::ranges::count(data, '=') + 1> rules;
-   std::size_t loc = 0;
-
-   constexpr auto ischar
-      = [](int c) { return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '_' || c == '-'; };
-   constexpr auto not_ws = [](int c) { return c != ' ' && c != '\t' && c != '\n'; };
-
-   auto start_loc = std::begin(data);
-   while (true) {
-      const auto equal_sign_loc = std::find(start_loc, std::end(data), '=');
-      if (equal_sign_loc == std::end(data)) {
-         break;
+   consteval comp_str(const std::array<char, N>& other) : data{}
+   {
+      for (std::size_t i = 0; i < N; ++i) {
+         data[i] = other[i];
       }
-      auto end_rule_loc = equal_sign_loc;
-      while (true) {
-         if (*end_rule_loc == ';') {
-            break;
-         }
-         else if (*end_rule_loc == '\'') {
-            do {
-               ++end_rule_loc;
-            } while (*end_rule_loc != '\'');
-         }
-         else if (*end_rule_loc == '"') {
-            do {
-               ++end_rule_loc;
-            } while (*end_rule_loc != '"');
-         }
-         ++end_rule_loc;
-      }
-
-      const auto mri = [](auto val) { return std::make_reverse_iterator(val); };
-
-      const auto print_start = std::find_if(start_loc, equal_sign_loc, ischar);
-      const auto print_end = std::find_if(mri(start_loc), mri(equal_sign_loc), ischar) + 1;
-
-      const auto rule_start = std::find_if(equal_sign_loc + 1, end_rule_loc, not_ws);
-      const auto rule_end = std::find_if(mri(equal_sign_loc + 1), mri(end_rule_loc), not_ws) + 1;
-      rules[loc] = {rule_start, rule_end.base()};
-      ++loc;
-      start_loc = end_rule_loc + 1;
    }
 
-   return rules;
-}
-
-struct rule_str {
-   std::string_view name;
-   std::string_view def;
-   int num_def_entities;
-};
-
-template<typename T, typename...>
-struct head {
-   using type = T;
+   char data[N];
+   constexpr auto operator<=>(const comp_str&) const = default;
 };
 
 template<auto... values>
@@ -99,9 +46,7 @@ constexpr auto is_ws = [](const char c) { return c == one_of<' ', '\n', '\r', '\
 template<typename E>
 struct unexpected {
 public:
-   constexpr unexpected(E err) noexcept
-      : error_{err}
-   {}
+   constexpr unexpected(E err) noexcept : error_{err} {}
 
    constexpr E error() const noexcept { return error_; }
 
@@ -112,23 +57,11 @@ private:
 template<typename T, typename E>
 struct const_expected {
 public:
-   constexpr const_expected() noexcept
-      : value_{{}}
-      , error_{{}}
-      , has_value_{false}
-   {}
+   constexpr const_expected() noexcept : value_{{}}, error_{{}}, has_value_{false} {}
 
-   constexpr const_expected(const T& val) noexcept
-      : value_{val}
-      , error_{}
-      , has_value_{true}
-   {}
+   constexpr const_expected(const T& val) noexcept : value_{val}, error_{}, has_value_{true} {}
 
-   constexpr const_expected(unexpected<E> err) noexcept
-      : value_{}
-      , error_{err.error()}
-      , has_value_{false}
-   {}
+   constexpr const_expected(unexpected<E> err) noexcept : value_{}, error_{err.error()}, has_value_{false} {}
 
    constexpr T value() const
    {
@@ -138,20 +71,11 @@ public:
       return value_;
    }
 
-   constexpr operator T() const noexcept
-   {
-      return value();
-   }
+   constexpr operator T() const noexcept { return value(); }
 
-   constexpr E error() const noexcept
-   {
-      return error_;
-   }
+   constexpr E error() const noexcept { return error_; }
 
-   constexpr explicit operator bool() const noexcept
-   {
-      return has_value_;
-   }
+   constexpr explicit operator bool() const noexcept { return has_value_; }
 
 private:
    [[no_unique_address]] T value_;
@@ -162,6 +86,12 @@ private:
 struct rule_err {
    const char* where;
    const char* why;
+};
+
+struct rule_str {
+   std::string_view name;
+   std::string_view def;
+   int num_def_entities = 0;
 };
 
 // TODO: Try and find a way to make errors propagate (eventually)
@@ -195,10 +125,15 @@ constexpr auto find_rule(std::string_view v) -> const_expected<std::pair<rule_st
    while (true) {
       const auto end_next = std::find_if_not(start_next, v.end(), is_ws);
       if (end_next == v.end()) {
-         return unexpected{rule_err{start_next, "Couldn't find end of rule (unexpected end of string; was a semi-colon forgotten?)"}};
+         return unexpected{
+            rule_err{start_next, "Couldn't find end of rule (unexpected end of string; was a semi-colon forgotten?)"}};
       }
       else if (*end_next == ';') {
-         return std::pair<rule_str, std::string_view>{{{start_rule, end_rule}, {start_def, end_next}, num_entities}, {end_next + 1, v.end()}};
+         if (paren_level != 0) {
+            return unexpected{rule_err{end_next, "Rule has opening parenthesis left unclosed"}};
+         }
+         return std::pair<rule_str, std::string_view>{
+            {{start_rule, end_rule}, {start_def, end_next}, num_entities}, {end_next + 1, v.end()}};
       }
       else if (*end_next == one_of<'"', '\'', '`', '[', '{'>) {
          start_next = end_next;
@@ -223,12 +158,14 @@ constexpr auto find_rule(std::string_view v) -> const_expected<std::pair<rule_st
       }
       else if (*end_next == '(') {
          paren_level += 1;
+         start_next = end_next + 1;
       }
       else if (*end_next == ')') {
          if (paren_level == 0) {
             return unexpected{rule_err{end_next, "Closing parenthesis with no matching opening one"}};
          }
          paren_level -= 1;
+         start_next = end_next + 1;
       }
       else if (is_alpha(*end_next)) {
          start_next = std::find_if_not(end_next, v.end(), is_alpha_or_other);
@@ -237,11 +174,15 @@ constexpr auto find_rule(std::string_view v) -> const_expected<std::pair<rule_st
          start_next = end_next + 1;
       }
       else {
-         return unexpected{rule_err{end_next, R"(Unexpected character in rule definition (must be within [[(a-zA-Z='"`|-+*]))"}};
+         return unexpected{
+            rule_err{end_next, R"(Unexpected character in rule definition (must be within [[(a-zA-Z='"`|-+*]))"}};
       }
       ++num_entities;
    }
 }
+
+template<comp_str line, comp_str why>
+struct error { void fail(); };
 
 template<comp_str str>
 constexpr auto decompose_rules_impl()
@@ -250,7 +191,9 @@ constexpr auto decompose_rules_impl()
    // This is ensured to be large enough to hold all rules
    constexpr auto oversize = std::ranges::count(v, ';');
 
-   constexpr auto num_rules_and_rules = [&]() constexpr -> const_expected<std::pair<int, std::array<rule_str, oversize>>, rule_err> {
+   constexpr auto num_rules_and_rules
+      = [&]() constexpr->const_expected<std::pair<int, std::array<rule_str, oversize>>, rule_err>
+   {
       int num_rules = 0;
       std::array<rule_str, oversize> rules{{}};
       const char* start_next = v.begin();
@@ -266,8 +209,9 @@ constexpr auto decompose_rules_impl()
          }
       }
       return std::pair<int, std::array<rule_str, oversize>>{num_rules, rules};
-   }();
-   if (num_rules_and_rules) {
+   }
+   ();
+   if constexpr (num_rules_and_rules) {
       constexpr auto num_rules = num_rules_and_rules.value().first;
       constexpr auto rules = num_rules_and_rules.value().second;
       std::array<rule_str, num_rules> to_ret;
@@ -275,12 +219,33 @@ constexpr auto decompose_rules_impl()
       return to_ret;
    }
    else {
-      ;
+      constexpr auto err_info = num_rules_and_rules.error();
+      constexpr auto line_and_err = [&]() constexpr {
+         // Create line of error
+         constexpr auto mri = [](auto val) { return std::make_reverse_iterator(val); };
+         constexpr auto null_or_newline = [](char c){return c == '\n' || c == '\0'; };
+         constexpr auto start_line_loc = std::find_if(mri(err_info.where), mri(v.begin()), null_or_newline).base();
+         constexpr auto end_line_loc = std::find_if(err_info.where, v.end(), null_or_newline);
+         std::array<char, std::distance(start_line_loc, end_line_loc)> line;
+         std::copy(start_line_loc, end_line_loc, line.data());
+         constexpr auto why_size = [&]() {
+            int i = 0;
+            for (; err_info.why[i] != '\0'; ++i) {}
+            return i;
+         }();
+         std::array<char, why_size> why;
+         for (int i = 0; i < why_size; ++i) {
+            why[i] = err_info.why[i];
+         }
+         return std::make_pair(line, why);
+      }();
+      error<line_and_err.first, line_and_err.second>{}.fail();
+      return std::array<rule_str, 1>{{}};
    }
 }
 
 template<comp_str str>
-constexpr auto decompose_rules = decompose_rules_impl<str>();
+inline constexpr auto decompose_rules = decompose_rules_impl<str>();
 
 int main()
 {
@@ -295,6 +260,7 @@ int main()
    static_assert(rule[1].num_def_entities == 3);
    static_assert(rule[2].name == "test");
    static_assert(rule[2].num_def_entities == 2);
+   
    constexpr auto rules = decompose_rules<R"ebnf(
       letter = [a-zA-Z] ;
       digit = [0-9] ;
@@ -316,7 +282,6 @@ int main()
    for (const auto& r : rules) {
       std::cout << r.name << ": " << r.num_def_entities << '\n';
    }
-   static_assert(rules[7].name == "rhs");
    std::cout << rules[7].num_def_entities << ' ' << rules[7].def.size() << '\n';
    // constexpr auto rules = test<R"ebnf(
    // letter = "A" | "B" | "C" | "D" | "E" | "F" | "G"
